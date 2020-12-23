ebdd324134ff1f8d5ec477ee79ec6f723a9a777d
naughtysoda <ctainny@gmail.com>
2020-12-23 15:54:06 +0700

    CraeteAdmin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import StaffProfile,CollectStar,CollectStaff
 
 # Register your models here.
+admin.site.register(StaffProfile)
+admin.site.register(CollectStar)
+admin.site.register(CollectStaff)
